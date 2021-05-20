a4d05699c7f7b3191cd68e8ecc57a0850acf3b30
Nitrej <52327738+Nitrej@users.noreply.github.com>
2021-05-20 18:43:19 +0200

    Shooting Delay

--- 2dTest/Assets/ShootingController.cs
+++ 2dTest/Assets/ShootingController.cs
@@ -7,16 +7,26 @@ public class ShootingController : MonoBehaviour
     public GameObject bullet;
     public Transform shootingPoint;
 
+    public float delay = 0.5f;
+    float timeBetweenSpawn = 0;
 
+    private void Start() {
+
+        float timeBetweenSpawn = delay;
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            GameObject newBullet = Instantiate(bullet, shootingPoint.position, Quaternion.identity);
-            Bullet blt = newBullet.GetComponent<Bullet>();
-            Vector2 bltMoveDir = (Vector2)newBullet.transform.position - (Vector2)transform.position;
-            blt.Setup(bltMoveDir.normalized);
+            if (timeBetweenSpawn <= 0) {
+                GameObject newBullet = Instantiate(bullet, shootingPoint.position, Quaternion.identity);
+                Bullet blt = newBullet.GetComponent<Bullet>();
+                Vector2 bltMoveDir = (Vector2)newBullet.transform.position - (Vector2)transform.position;
+                blt.Setup(bltMoveDir.normalized);
+                timeBetweenSpawn = delay;
+            }
+            timeBetweenSpawn -= Time.deltaTime;
         }
     }
 }
